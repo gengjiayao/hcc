8875c5397f691e52ace48adffd6668cbb2b984d1
gengjiayao <1098469310@qq.com>
2025-11-27 19:57:20 +0800

    fix: int.

--- src/point-to-point/model/rdma-hw.cc
+++ src/point-to-point/model/rdma-hw.cc
@@ -349,8 +349,8 @@ int RdmaHw::ReceiveUdp(Ptr<Packet> p, CustomHeader &ch) {
         seqh.SetSport(ch.udp.dport);
         seqh.SetDport(ch.udp.sport);
 
-        // check nhop, if more than 1, remove the last hop info
-        if (ch.udp.ih.nhop > 1) {
+        // check nhop, if more than 0, remove the last hop info
+        if (ch.udp.ih.nhop > 0) {
             int last_hop = --ch.udp.ih.nhop;
             memset(&ch.udp.ih.hop[last_hop], 0, sizeof(ch.udp.ih.hop[last_hop]));
         }
