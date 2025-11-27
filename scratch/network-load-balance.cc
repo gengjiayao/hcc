3c6d0ef6dd1f360cb22d6c0331dd0abe80705296
gengjiayao <1098469310@qq.com>
2025-11-27 17:47:12 +0800

    add: leaf_spine_8_OS1

--- scratch/network-load-balance.cc
+++ scratch/network-load-balance.cc
@@ -1403,6 +1403,7 @@ int main(int argc, char *argv[]) {
 
     // manually type BDP
     std::map<std::string, uint32_t> topo2bdpMap;
+    topo2bdpMap[std::string("leaf_spine_8_100G_OS1")] = 104000;  // RTT=8320 --> all 100G links
     topo2bdpMap[std::string("leaf_spine_8_100G_OS4")] = 104000;  // RTT=8320 --> all 100G links
     topo2bdpMap[std::string("leaf_spine_128_100G_OS2")] = 104000;  // RTT=8320 --> all 100G links
     topo2bdpMap[std::string("leaf_spine_64_100G_OS1")] = 104000;  // RTT=8320 --> all 100G links
